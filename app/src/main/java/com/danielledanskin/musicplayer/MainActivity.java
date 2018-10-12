597545b5279ab3a6099fc2689de0685bed5eeb52
Danielle Danskin <ddanskin@users.noreply.github.com>
2018-10-12 14:32:32 -0400

    Add Bells mp3 file to resources and created start and stop methods for playing the music file.

--- app/src/main/java/com/danielledanskin/musicplayer/MainActivity.java
+++ app/src/main/java/com/danielledanskin/musicplayer/MainActivity.java
@@ -1,13 +1,25 @@
 package com.danielledanskin.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.bells_tibetan_daniel_simon);
+    }
+
+    public void startMusic(View view) {
+        mediaPlayer.start();
+    }
+
+    public void stopMusic(View view) {
+        mediaPlayer.pause();
     }
 }
