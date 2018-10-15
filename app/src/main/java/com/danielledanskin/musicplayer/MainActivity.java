20a33837df1c6313d4f57643038b24df1cf205c3
Danielle Danskin <ddanskin@users.noreply.github.com>
2018-10-15 18:04:51 -0400

    Edit click events to be handled with onClickListener. Added onCompletionListener with toast notifying user song has ended.

--- app/src/main/java/com/danielledanskin/musicplayer/MainActivity.java
+++ app/src/main/java/com/danielledanskin/musicplayer/MainActivity.java
@@ -4,6 +4,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
@@ -13,17 +15,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.bells_tibetan_daniel_simon);
+        Button startButton = (Button) findViewById(R.id.start_button);
+        Button stopButton = (Button) findViewById(R.id.stop_button);
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.seekTo(0);
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(getApplicationContext(), "song finished!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    public void startMusic(View view) {
-        mediaPlayer.start();
-    }
-
-    public void stopMusic(View view) {
-        mediaPlayer.pause();
-    }
-
-    public void resetMusic(View view) {
-        mediaPlayer.seekTo(0);
-    }
 }
